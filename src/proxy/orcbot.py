ca4d579d1fe26b9330c800117e5e77d3f631706d
harahauk <harahauk@SOROS>
2010-03-16 14:43:10 +0100

    added verification from Runas code to orcbot

--- src/proxy/orcbot.py
+++ src/proxy/orcbot.py
@@ -15,6 +15,11 @@ the authors permission, but in compliance with the license terms of irclib.
 This is meant as a temporary solution till the Event handler and ServerConnectionDaemon
 is written.
 '''
+# The with statement is not available in Python 2.5, so import it
+from __future__ import with_statement
+import GnuPGInterface
+import random
+
 from ircbot import SingleServerIRCBot
 from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad, ip_quad_to_numstr
 
@@ -24,9 +29,88 @@ class Orcbot:
     def __init__(self, parent):
         # This dict establishes whether a user is currently undergoing a validation process
         self.validation_in_progress = dict()
+        # This dictionary contains the validated users, an
+        self.validated_users
         self.parent = parent
         #self.gpgverifier = verify_sign 
+    def validate_pseudonym(self, pseudonym, certificate):
+        '''
+        Shamelesslys stolen Runa's code and fitted it for Orcbot
+        Takes the argument pseudonym and performs validation on the 
+        pseudonym against the cert.
+        # TODO test and debug
+        '''                
+        # Define GnuPGInterface
+        gnupg = GnuPGInterface.GnuPG()
+        
+        ### Start config ###
+        
+        # Tell gnupg where to find the GPG keyring. That is, the .gnupg
+        # directory. Do not add the trailing slash.
+        gnupg.options.homedir = ""
+        
+        # The ID of the key that was used to sign the user input.
+        keyid = ""
+        
+        ### End config ###
+        
+        """Verify signature of user input using GPG"""
+        # Read signature from user input
+        user_input = []
+        entry = raw_input("Enter signature, 'done' on its own line when you are finished: \n")
+        #while entry != "done":
+        #    user_input.append(entry)
+        #    entry = raw_input("")
+        #user_input = '\n'.join(user_input)
+        
+        #overwrites the while loop
+        user_input = pseudonym
+        
+        # Write user input to a random file in /tmp, so that the signature
+        # can be verified later on. Use small letters, big letters and
+        # digits for the filename.
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        letters += letters.upper () + "0123456789"
+    
+        # Actually write user input to file
+        input_file = "/tmp/"
+        for i in range(10):
+            input_file += random.choice(letters)
+        with open(input_file, 'w') as write_input:
+            write_input.write(user_input)
+    
+        # Open the file to read the contents
+        verify = open(input_file, 'r')
+    
+        # Verify the signature
+        clearsign = gnupg.run(['--no-tty'], ['--decrypt'],
+        create_fhs=['stdout','stderr'], attach_fhs={'stdin': verify})
+    
+        # Read the output from gnupg, and split it into an array so that it
+        # can be evaluated.
+        signature = clearsign.handles['stderr'].read()
+        s = signature.split()
+    
+        # The signature can not be checked if the public key is not found
+        if "public key not found" in signature:
+            print "It seems like the key has not been imported"
+            return False
+    
+        # Check if the signature is valid. Do not accept a bad signature.
+        if "BAD" in signature:
+            print "Sorry, this signature is not valid"
+            return False
+        
+        # Accept a good signature if it is signed by the right key ID. If it
+        # is a good signature, and the right key ID have been used, check
+        # when the signature was made.
+        if "Good" in signature and keyid in s[14]:
+                print "Good signature made", s[4], s[5], s[6], s[7], s[8], s[9]
+                return True
+        
+
     def get_certificate(self, cmd, nick):
+        # Adds more of the pseudonym to the dictionary.
         self.validation_in_progress[nick] += cmd
         
     def do_command(self, cmd, c, nick):
@@ -42,15 +126,15 @@ class Orcbot:
                 # Used for debugging
                 c.privmsg(nick, "Your certificate was retrieved, it was " + self.validation_in_progress.get(nick))
                 #TODO sanitize input
-                #pseudonym = self.gpgverifier(self.validation_in_progress.get(nick))
-                # Calls the verifyer, untested. Sets true for debug.
-                pseudonym = "Fakepseudonymfornow"
+                validation_result = self.validate_pseudonym(self.validation_in_progress.get(nick), certificate)
                 # We have a pseudonym or a empty string, remove the nick from the validation dictionary as that process is done
                 del self.validation_in_progress[nick]
-                if(len(pseudonym) > 0):
+                if(validation_result):
+                    c.privmsg(nick, "Validation performed succesfully, you may now connect")
                     #parent.add_pseudonym_to_validated_users()
                     # TODO add to validated users and allow user to connect
-                    return
+                else:
+                    c.privmsg(nick, "Validation failed, check that you are using a valid pseudonym.")
         elif (cmd=="connect"):
             #self.parent.validatedusers.haskey(nick)
             return            
@@ -71,7 +155,9 @@ class Orcbot:
             c.privmsg(nick, "The process is concluded by typing 'done'. on a single line.")
             #TODO
         elif (cmd=="help connect"):
-            c.privmsg(nick, "Connects you to an irc server of your choice:")
+            c.privmsg(nick, "Connects you to an irc server of your choice.")
+            c.privmsg(nick, "The command may take one or two arguments, servername and port")
+            c.privmsg(nick, "If no port is defined, the command wil try connecting on the standard port.")
             #TODO
         elif (cmd=="die"):
             # Used for debugging
