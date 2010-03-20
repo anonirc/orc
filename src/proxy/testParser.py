da9f4f16a8dc9ed801daf419711844e97f75f7fd
Lars <lars@littlelaptop.(none)>
2010-03-20 19:45:33 +0100

    Added the IrcParser and tester for the ircparser

---
+++ src/proxy/testParser.py
@@ -0,0 +1,30 @@
+import sys
+import thread
+import socket
+import string
+import time
+
+from ircParse import *
+
+
+HOST=socket.gethostname()
+PORT=31336
+
+S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+S.bind((HOST,PORT))
+S.listen(5)
+
+sender=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sender.connect((HOST, PORT))
+
+conn=S.accept()
+
+def send_Recv_and_Parse(sender, receiver):
+    while 1:      
+        #TODO: make this a variety of RFC compliant messages
+        sender.send(":guntbert!~re@unaffiliated/guntbert PRIVMSG #ubuntu :enthus: like histo just said: carry the app and all its dependencies")
+        process_data(receiver).printC()
+        time.sleep(2)
+
+thread.start_new_thread(send_Recv_and_Parse, (sender, conn))
+
