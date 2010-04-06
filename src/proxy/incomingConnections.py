c8bac9696adbcc4f3e1f85d70949a36fea05b36d
Lars <lars@littlelaptop.(none)>
2010-04-06 09:15:51 +0200

    Added a wait timer, and a timeout so that the event handling should be less blocking

--- src/proxy/incomingConnections.py
+++ src/proxy/incomingConnections.py
@@ -3,6 +3,7 @@
 import threading
 import thread
 import socket
+import time
 
 import ircParse as parse
 
@@ -36,21 +37,22 @@ class IncomingConnectionDaemon(threading.Thread):
         SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         SOCK.bind((self.host,self.port))
         SOCK.listen(self.backlog)
-
+        
         orcbot = SOCK.accept()
         print "OrcBot connected"
         print orcbot
+        orcbot[0].settimeout(.5)
         #starts the method that looks for events in connections
         spawn_look_for_events(self.host, connections, orcbot)
         #first connection should be from orcbot
         
         while 1:
-            connections[SOCK.accept()]= None
-            print("connected")
-
-def get_orcbot():
-    return orcbot
-            
+            try:
+                connections[SOCK.accept()]= None
+                print("connected")
+            finally:
+                time.sleep(1)
+                
 def spawn_look_for_events(lhost, lconnections, orcbot):
     """Starts the look_for_events function in  new thread
     and passes connections to it
@@ -66,17 +68,22 @@ def look_for_events(host, lconnections, orcbot):
     Arguments: host, connectxions
     """
     while 1:
+        #TODO: remove this when the app doesnt kill all performance anymore
+        time.sleep(1)
         if(lconnections):
             print "loopin'"
-            print orcbot
-            print
             #goes through connections and calls ircParse's process_data
             #on them, returning an Event object, or None
             events = map(lambda x:parse.process_data(x, orcbot), lconnections.items())
             #filters out the None's, in other words the ones without new data
             events = filter(lambda x:x!=None, events)
-            for ev in events:
-                ev.apply_handler()
+            for socket_events in events:
+                for socket_event in socket_events:
+                    socket_event.apply_handler()
+            orcbot_events = parse.process_data((orcbot, None), orcbot)
+            if orcbot_events:
+                for orcbot_event in orcbot_events:
+                    orcbot_event.apply_handler()
         
 def add_target(connection, target):
     connections[connection]=target
